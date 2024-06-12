229ee39ef92de6e10ef716809b06f22a2ebc1732
TOODDS <nogueiralimajoaopedro@gmail.com>
2024-06-12 10:05:48 -0300

    Questão Corrigida

--- Q07.py
+++ Q07.py
@@ -1,7 +1,7 @@
 nome = input("Nome do estudante: ")
 materia = input("Qual é a matéria?: ")
-n1 = int(input("Digite sua primeira nota: "))
-n2 = int(input("Digite sua segunda nota: "))
+n1 = float(input("Digite sua primeira nota: "))
+n2 = float(input("Digite sua segunda nota: "))
 m = (n1 + n2)/2
 if m >= 6:
     situacao = "aprovado"
